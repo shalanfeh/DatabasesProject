96f991ad3df61f9f53327b5a7b7343eb4155866d
Ismail <ismail.shalanfeh@gmail.com>
2026-03-16 16:26:19 -0700

    Updated ServerAbstract to take in parameters

--- src/ServerAPI/ServerAbstract.java
+++ src/ServerAPI/ServerAbstract.java
@@ -11,9 +11,11 @@ Must Start and End a transaction!
  */
 package ServerAPI;
 
+import java.util.Map;
+
 public abstract class ServerAbstract {
     //called by client API
-    abstract public String Execute();
+    abstract public String Execute(Map<String, Object> Parameters);
 
     //The prepare statement -- Bobby tables
     abstract protected void Prepare();
